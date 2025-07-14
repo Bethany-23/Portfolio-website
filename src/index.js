276f5392a000e7240e4326de2bd3fbb05dbd8646
Bethany-23 <bethanyaddis7@gmail.com>
2025-07-14 13:11:16 +0300

    removed basename

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router basename="/Portfolio-website">
+    <Router>
       <App />
     </Router>
   </React.StrictMode>
